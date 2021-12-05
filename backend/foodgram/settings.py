0151617a9d5c2a080258c1f1161e7d4ebc8af358
krytet <kryt.2000@gmail.com>
2021-12-05 22:08:47 +0600

    fix docker and nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -140,10 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static_backend/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_backend")
 
 MEDIA_URL = '/media_backend/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_backend')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
